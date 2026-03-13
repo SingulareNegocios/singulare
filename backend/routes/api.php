c825fcb017aac3f5581bd7b744188151e8606ad8
Igor-Barreto <igor_barreto2003@hotmail.com>
2026-03-13 20:59:45 -0300

    ✨feat: add crud MyHistory

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\MyHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -14,10 +15,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 Route::middleware(['auth:sanctum', 'can:admin'])->group(function () {
     Route::apiResource('/users', UserController::class);
+    Route::apiResource('/my-history', MyHistory::class)->except('index', 'show');
 });
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/my-history', [MyHistory::class, 'index'] );
+Route::get('/my-history/{id}', [MyHistory::class, 'show'] );
 
 require __DIR__.'/auth.php';
